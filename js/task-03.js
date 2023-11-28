6676a4116b9dcc19cd0d939001b484d02f732d72
CosmicSand <skyjlen@gmail.com>
2023-11-28 18:38:08 +0200

    Third

--- js/task-03.js
+++ js/task-03.js
@@ -3,21 +3,10 @@
 const inp = document.querySelector('#name-input');
 const spn = document.querySelector('#name-output');
 
-// const handle = () => {
-//   spn.insertAdjacentHTML('afterbegin', inp.textContent);
-// };
-
-// inp.addEventListener('keydown', event => {
-//   if (event.code !== 'Space') {
-//     console.log(inp.textContent);
-//     spn.textContent = inp.textContent.trim();
-//   }
-//   {
-//   }
-// });
-
 inp.addEventListener('input', event => {
-  if (event.code !== 'Space') {
-    spn.textContent = event.currentTarget.value;
+  const username = event.currentTarget.value.trim().split('');
+
+  if (username.length !== 0) {
+    spn.textContent = username.join('');
   }
 });
