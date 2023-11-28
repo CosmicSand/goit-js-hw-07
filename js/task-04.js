44aa434c656479ac2b37d625a5328926395ef8de
CosmicSand <skyjlen@gmail.com>
2023-11-28 20:41:17 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,3 +1,22 @@
 'use strict';
 
-const inp = document.querySelector('#name-input');
+const registerForm = document.querySelector('.login-form');
+
+registerForm.addEventListener('submit', register);
+
+function register(event) {
+  event.preventDefault();
+  const form = event.target;
+  const login = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
+
+  if (login.length === 0 || password.length === 0) {
+    alert('All form fields must be filled in');
+  } else {
+    const first = form.elements.email.getAttributes('type');
+    const second = form.elements.password.getAttributes('type');
+    const result = { first: login, second: password };
+  }
+
+  event.target.reset();
+}
